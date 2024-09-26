a4640a996ff5898929c002d7d5e63a534b733ba5
joshua.burkhan <bai21030@byui.edu>
2024-09-25 19:14:09 -0500

    week02 tasks

---
+++ week02/script/temples.js
@@ -0,0 +1,9 @@
+const navigation = document.querySelector('.navigation');
+const hamButton = document.querySelector('#menu');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+document.getElementById("last-modified").textContent = document.lastModified;
