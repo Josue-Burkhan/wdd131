263c76c8eac3753a1124347d270d00c49184b1c3
joshua.burkhan <bai21030@byui.edu>
2024-10-01 19:40:59 -0500

    last changes place week3

--- scripts/place.js
+++ scripts/place.js
@@ -3,13 +3,13 @@ document.getElementById("last-modified").textContent = document.lastModified;
 
 
 let imgOriginal = true;
-document.getElementById("change-img").addEventListener("click", function(){
-    if(imgOriginal) {
+document.getElementById("change-img").addEventListener("click", function () {
+    if (imgOriginal) {
         document.getElementById("large-img").setAttribute("srcset", "images/Puno-picture-1-desktop-large.webp")
         document.getElementById("medium-img").setAttribute("srcset", "images/Puno-picture-1-desktop-medium.webp")
         document.getElementById("small-img").setAttribute("srcset", "images/Puno-picture-1-mobile-small.webp")
     }
-    else{
+    else {
         document.getElementById("large-img").setAttribute("srcset", "images/Puno-picture-desktop-large.webp")
         document.getElementById("medium-img").setAttribute("srcset", "images/Puno-picture-desktop-medium.webp")
         document.getElementById("small-img").setAttribute("srcset", "images/Puno-picture-mobil-small.webp")
@@ -19,9 +19,9 @@ document.getElementById("change-img").addEventListener("click", function(){
 
 function calculateWindChill(tempF, windSpeedMph) {
     return (
-        35.74 + 
-        0.6215 * tempF - 
-        35.75 * Math.pow(windSpeedMph, 0.16) + 
+        35.74 +
+        0.6215 * tempF -
+        35.75 * Math.pow(windSpeedMph, 0.16) +
         0.4275 * tempF * Math.pow(windSpeedMph, 0.16)
     ).toFixed(2);
 }
