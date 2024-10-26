2a49d85f6e82f31b3909c2de26c3137f8bc863e4
joshua.burkhan <bai21030@byui.edu>
2024-10-26 09:04:57 -0500

    iiiii

--- project/Wild-Fantasy.com/scripts/home.js
+++ project/Wild-Fantasy.com/scripts/home.js
@@ -13,20 +13,23 @@ menuBut.addEventListener('click', () => {
 
 
 //FAQ section
-headers.forEach(header => {
-  header.addEventListener('click', () => {
-    const currentActive = document.querySelector('.faq-header.active');
-
-    if (currentActive && currentActive !== header) {
-      currentActive.classList.remove('active');
-      currentActive.nextElementSibling.classList.remove('show');
-    }
-
-    header.classList.toggle('active');
-    const content = header.nextElementSibling;
-    content.classList.toggle('show');
+function faqSection () {
+  headers.forEach(header => {
+    header.addEventListener('click', () => {
+      const currentActive = document.querySelector('.faq-header.active');
+  
+      if (currentActive && currentActive !== header) {
+        currentActive.classList.remove('active');
+        currentActive.nextElementSibling.classList.remove('show');
+      }
+  
+      header.classList.toggle('active');
+      const content = header.nextElementSibling;
+      content.classList.toggle('show');
+    });
   });
-});
+}
+
 
 
 
